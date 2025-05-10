caee073017de745ce3e88bf5a6032b773adb433c
eonjong0218 <djswhd1234@naver.com>
2025-05-10 23:44:53 +0900

    chore: update main.dart route setup

---
+++ frontend/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(const ReviewEatApp());
+}
+
+class ReviewEatApp extends StatelessWidget {
+  const ReviewEatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'REVIEWEAT',
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/login': (context) => const LoginScreen(),
+      },
+    );
+  }
+}
