6f88e6b93a51ec2cda077a71e2565c0a3e93257c
eonjong0218 <djswhd1234@naver.com>
2025-05-24 14:34:25 +0900

    add get_user_by_username function to crud.py to fix registration error

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -7,6 +7,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = pwd_context.hash(user.password)
     db_user = models.User(email=user.email, username=user.username, hashed_password=hashed_password)
