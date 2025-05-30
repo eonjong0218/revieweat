e7671116d7896830aa9a2cc1fa577bad0e21f5c4
eonjong0218 <djswhd1234@naver.com>
2025-05-30 12:39:19 +0900

    /review_success 경로에 대해 리뷰 정보 전달 및 유효성 체크 로직 구현

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -7,6 +7,7 @@ import 'screens/search_result_screen.dart';
 import 'screens/review_place_search_screen.dart';
 import 'screens/review_second_screen.dart';
 import 'screens/review_final_screen.dart';
+import 'screens/review_success_screen.dart';
 import 'screens/profile_screen.dart';
 
 void main() {
@@ -39,13 +40,25 @@ class ReviewEatApp extends StatelessWidget {
       initialRoute: '/',
       onGenerateRoute: (settings) {
         if (settings.name == '/') {
-          return MaterialPageRoute(builder: (_) => const SplashScreen());
+          return MaterialPageRoute(
+            builder: (_) => const SplashScreen(),
+            settings: settings,
+          );
         } else if (settings.name == '/login') {
-          return MaterialPageRoute(builder: (_) => const LoginScreen());
+          return MaterialPageRoute(
+            builder: (_) => const LoginScreen(),
+            settings: settings,
+          );
         } else if (settings.name == '/register') {
-          return MaterialPageRoute(builder: (_) => const RegisterScreen());
+          return MaterialPageRoute(
+            builder: (_) => const RegisterScreen(),
+            settings: settings,
+          );
         } else if (settings.name == '/home') {
-          return MaterialPageRoute(builder: (_) => const HomeScreen());
+          return MaterialPageRoute(
+            builder: (_) => const HomeScreen(),
+            settings: settings,
+          );
         } else if (settings.name == '/search_result') {
           final args = settings.arguments;
           String initialQuery = '';
@@ -54,6 +67,7 @@ class ReviewEatApp extends StatelessWidget {
           }
           return MaterialPageRoute(
             builder: (_) => SearchResultScreen(initialQuery: initialQuery),
+            settings: settings,
           );
         } else if (settings.name == '/review_place_search') {
           final args = settings.arguments;
@@ -63,18 +77,21 @@ class ReviewEatApp extends StatelessWidget {
           }
           return MaterialPageRoute(
             builder: (_) => ReviewPlaceSearchScreen(keyword: keyword),
+            settings: settings,
           );
         } else if (settings.name == '/review_second') {
           final args = settings.arguments;
           if (args is Map<String, dynamic>) {
             return MaterialPageRoute(
               builder: (_) => ReviewSecondScreen(place: args),
+              settings: settings,
             );
           } else {
             return MaterialPageRoute(
               builder: (_) => const Scaffold(
                 body: Center(child: Text('잘못된 접근입니다.')),
               ),
+              settings: settings,
             );
           }
         } else if (settings.name == '/review_final') {
@@ -91,19 +108,52 @@ class ReviewEatApp extends StatelessWidget {
                 selectedRating: args['selectedRating'],
                 selectedCompanion: args['selectedCompanion'],
               ),
+              settings: settings,
             );
           } else {
             return MaterialPageRoute(
               builder: (_) => const Scaffold(
                 body: Center(child: Text('리뷰 최종화면 접근 오류')),
               ),
+              settings: settings,
+            );
+          }
+        } else if (settings.name == '/review_success') {
+          final args = settings.arguments;
+          if (args is Map<String, dynamic> &&
+              args['place'] != null &&
+              args['selectedDate'] != null &&
+              args['selectedRating'] != null &&
+              args['selectedCompanion'] != null &&
+              args['reviewText'] != null &&
+              args['images'] != null) {
+            return MaterialPageRoute(
+              builder: (_) => ReviewSuccessScreen(
+                place: args['place'],
+                selectedDate: args['selectedDate'],
+                selectedRating: args['selectedRating'],
+                selectedCompanion: args['selectedCompanion'],
+                reviewText: args['reviewText'],
+                images: args['images'],
+              ),
+              settings: settings,
+            );
+          } else {
+            return MaterialPageRoute(
+              builder: (_) => const Scaffold(
+                body: Center(child: Text('리뷰 완료화면 접근 오류')),
+              ),
+              settings: settings,
             );
           }
         } else if (settings.name == '/profile') {
-          return MaterialPageRoute(builder: (_) => const ProfileScreen());
+          return MaterialPageRoute(
+            builder: (_) => const ProfileScreen(),
+            settings: settings,
+          );
         }
         return null;
       },
     );
   }
-} 
+}
